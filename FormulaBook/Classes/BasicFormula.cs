7806d2451a945712e0916f2465ce326ce6b3afa9
Graham Hoggan <grahambentleyhoggan@gmail.com>
2025-05-27 07:45:27 -0700

    Started Complex Formula Support
    TODO: Solving Complex Equations
    I have no idea how.

--- FormulaBook/Classes/BasicFormula.cs
+++ FormulaBook/Classes/BasicFormula.cs
@@ -67,15 +67,35 @@ namespace FormulaBook.Classes
 
             return true;
         }
-        public string[] GetLeftElements()
+        public string[] GetLeftElements(bool removeDups = true)
         {
             if (!isValid()) return new string[0];
-            return RemoveDups(Compressed().Split("=")[0].Split("*"));
+            if (IsSimple())
+            {
+                if (removeDups)
+                    return RemoveDups(Compressed().Split("=")[0].Split("*"));
+                else
+                    return Compressed().Split("=")[0].Split("*");
+            }
+
+
+            return RemoveDups(ExpressionSplit(Compressed().Split("=")[0]));
+
+
+
         }
-        public string[] GetRightElements()
+        public string[] GetRightElements(bool removeDups = true)
         {
             if (!isValid()) return new string[0];
-            return RemoveDups(Compressed().Split("=")[1].Split("*"));
+            if (IsSimple())
+            {
+                if (removeDups)
+                    return RemoveDups(Compressed().Split("=")[1].Split("*"));
+                else
+                    return Compressed().Split("=")[1].Split("*");
+            }
+            return RemoveDups(ExpressionSplit(Compressed().Split("=")[1]));
+
         }
         public string[] GetElements()
         {
@@ -110,57 +130,65 @@ namespace FormulaBook.Classes
         }
         public double Solve(Dictionary<String,double> elements,string ElementToSolveFor)
         {
-            int countOfSolve = 0;
-            double RunningProduct = 1;
-            foreach(String element in Compressed().Split("=")[0].Split("*"))
+            if(IsSimple())
             {
-                if(element==ElementToSolveFor)
+                int countOfSolve = 0;
+                double RunningProduct = 1;
+                foreach (String element in GetLeftElements(false))
                 {
-                    countOfSolve++;
-                }
-                else
-                {
-                    double constant;
-                    if (double.TryParse(element, out constant))
+                    if (element == ElementToSolveFor)
                     {
-                        if (constant != 0)
-                        RunningProduct/=constant;
+                        countOfSolve++;
                     }
                     else
                     {
-                        if (elements[element] == 0) return 0;
-                        RunningProduct /= elements[element];
-                    }
+                        double constant;
+                        if (double.TryParse(element, out constant))
+                        {
+                            if (constant != 0)
+                                RunningProduct /= constant;
+                        }
+                        else
+                        {
+                            if (elements[element] == 0) return 0;
+                            RunningProduct /= elements[element];
+                        }
 
+                    }
                 }
-            }
-            foreach (String element in Compressed().Split("=")[1].Split("*"))
-            {
-                if (element == ElementToSolveFor)
+                foreach (String element in GetRightElements(false))
                 {
-                    countOfSolve--;
-                }
-                else
-                {
-                    double constant;
-                    if (double.TryParse(element, out constant))
+                    if (element == ElementToSolveFor)
                     {
-                        RunningProduct *= constant;
+                        countOfSolve--;
                     }
                     else
                     {
-                        RunningProduct *= elements[element];
+                        double constant;
+                        if (double.TryParse(element, out constant))
+                        {
+                            RunningProduct *= constant;
+                        }
+                        else
+                        {
+                            RunningProduct *= elements[element];
+                        }
                     }
                 }
-            }
-            if (countOfSolve == 0)
-            {
-                return 0;
+                if (countOfSolve == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return Round3(Math.Pow(RunningProduct, 1.0 / countOfSolve));
+                }
             }
             else
             {
-                return Round3(Math.Pow(RunningProduct, 1.0 / countOfSolve));
+                return ComplexSolve(elements, ElementToSolveFor);
             }
+           
         }
         public static double Round3(double value)
         {
@@ -188,6 +216,66 @@ namespace FormulaBook.Classes
 
             return path;
 
+        }
+        private bool IsSimple()
+        {
+            if (formula.Contains('/')) return false;
+            if (formula.Contains('+')) return false;
+            if (formula.Contains('-')) return false;
+            if (formula.Contains('^')) return false;
+            return true;
+        }
+        private string[] ExpressionSplit(string expression)
+        {
+            List<string> elements = new List<string>();
+            string[] multSplit = expression.Split("*");
+            foreach (string s in multSplit)
+            {
+                string[] plusSplit = s.Split("+");
+                foreach (string s2 in plusSplit)
+                {
+                    string[] minusSplit = s2.Split("-");
+                    foreach (string s3 in minusSplit)
+                    {
+                        string[] divSplit = s3.Split("/");
+                        foreach (string s4 in divSplit)
+                        {
+                            string[] expSplit = s4.Split("^");
+                            foreach (string s5 in expSplit)
+                            {
+                                string[] openSplit = s5.Split("(");
+                                foreach (string s6 in openSplit)
+                                {
+                                    string[] closeSplit = s6.Split(")");
+                                    foreach (string s7 in closeSplit)
+                                    {
+                                        if (s7.Length > 0) 
+                                        elements.Add(s7);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return elements.ToArray();
+        }
+        private double ComplexSolve(Dictionary<String, double> elements, string ElementToSolveFor)
+        {
+            string equation = Compressed();
+            foreach(string element in GetElements())
+            {
+                if(double.TryParse(element, out _))
+                {
+                    
+                }
+                else
+                {
+                    equation = equation.Replace(element, elements[element].ToString());
+                }
+            }
+            Equation e = new Equation(equation,ElementToSolveFor);
+            return e.Solve();
         }
     }
 }
---
+++ FormulaBook/Classes/Equation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaBook.Classes
+{
+    public class Equation
+    {
+        private string equation;
+        private string unknown;
+        public Equation(string equation, string unknownVariable)
+        {
+            this.equation = equation;
+            unknown = unknownVariable;
+        }
+        public double Solve()
+        {
+            return -1;
+        }
+    }
+}
